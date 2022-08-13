5b13b6481f8983248a713f34a621845b92e5ee19
chenxuyuan <chenxuyuan@yazx.com>
2022-08-13 19:12:27 +0800

    fix index html tag remove quote

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 cp ./static/lemon.ico ./themes/hugo-theme-next/static/imgs/icons/
 cp ./static/lemon.jpg ./themes/hugo-theme-next/static/imgs/
 hugo --minify
+sed -i 's/baidu-site-verification/"baidu-site-verification"/' ./public/index.html
+sed -i 's/msvalidate.01/"msvalidate.01"/' ./public/index.html
+sed -i 's/google-site-verification/"google-site-verification"/' ./public/index.html
