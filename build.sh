f6fb5b7865096326b3e651e838cf6aaf3989c0d9
lemon-1997 <lemon_1997@126.com>
2023-03-18 13:24:28 +0800

    fix SEO bug

--- build.sh
+++ build.sh
@@ -9,9 +9,13 @@ sed -i 's/element.querySelector('\''url'\'').textContent/element.querySelector('
 # gen static
 hugo --minify
 # fix tag quote
-sed -i 's/baidu-site-verification/"baidu-site-verification"/' ./public/index.html
-sed -i 's/msvalidate.01/"msvalidate.01"/' ./public/index.html
-sed -i 's/google-site-verification/"google-site-verification"/' ./public/index.html
+#sed -i 's/baidu-site-verification/"baidu-site-verification"/' ./public/index.html
+#sed -i 's/msvalidate.01/"msvalidate.01"/' ./public/index.html
+#sed -i 's/google-site-verification/"google-site-verification"/' ./public/index.html
+
+
+sed -i 's/<meta charset=utf-8>/<meta charset=utf-8><meta name="msvalidate.01" content="3043D1C99FE5A3EB5DB6C47B92C08392" \/><meta name="google-site-verification" content="pOFnbCaHzo9V2sRmz84rdH_SQrHlG3JbMiUDwLswDEE" \/>/' ./public/index.html
+
 # seo url
 cp ./public/sitemap.xml ./public/sitemap-cf.xml
 sed -i 's/lemon-1997.github.io/lemon-1997.pages.dev/g' ./public/sitemap-cf.xml
